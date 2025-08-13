f1354e8fd027b1d6ae86d2f94f3f8c894825c594
shrymhty <shreya.official93@gmail.com>
2025-08-13 18:21:09 +0530

    contact almost done, links to add

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import './Contact.css'
-import { map_pin, plus_icon } from '../../assets/asset'
-import map from "../../assets/map.webp"
-import { linkedin, github} from '../../assets/asset'
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
-const Contact = ({ activeSection }) => {
-  const isActive = activeSection === 'contact';
+const Contact = () => {
+
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
@@ -21,65 +18,140 @@ const Contact = ({ activeSection }) => {
 
   return (
     <div className="contact">
-      <div className={`contact-form ${isActive ? 'animate' : ''}`}>
-        
-        {/* Crosshair 1 */}
-        <div className="crosshair one">
-          <img src={plus_icon} alt="crosshair" />
-        </div>
-
-        {/* Content wrapper */}
-        <div className="form-content">
-          <p>Get In Touch</p>
-          <form className="contact-input">
-            <label htmlFor="input-name">Name</label>
-            <input type="text" id='input-name' placeholder='Your name' name='input-name'/>
-
-            <label htmlFor="input-email">Email</label>
-            <input type="email" placeholder='example@yourmail.com' name='input-email' id='input-email'/>
-
-            <label htmlFor="input-subject">Subject</label>
-            <input type="text" name="input-subject" id="input-subject" placeholder='Title'/>
-
-            <label htmlFor="input-message">Message</label>
-            <textarea name="input-message" id="input-message" placeholder='Type Here...'></textarea>
-          </form>
-        </div>
-
-        {/* Crosshair 2 */}
-        <div className="crosshair two">
-          <img src={plus_icon} alt="crosshair" />
-        </div>
-
-      </div>
-
-      <div className="other-details">
-        <p><span>Got an idea, a question, or just want to say hi?</span><br/>Feel free to drop me a message.</p>
-        <div className="social-links">
-          <a 
-            className="link"
-            href="https://linkedin.com/in/shreyamhty"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={linkedin} alt="" />
-            <p>linkedin/shreyamhty</p>
-          </a>
-          <a 
-            className="link"
-            href="https://github.com/shrymhty"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={github} alt="" />
-            <p>github/shrymhty</p>
-          </a>
-        </div>
-        <div className="link">
-            <img src={map_pin} alt="" />
-            <p>Bengaluru, Karnataka<br/>{time.toLocaleTimeString()}</p>
+      <div className='contact-div'>
+        <div className="contact-terminal">
+          <p className="term-header">comms_terminal.exe</p>
+          <div className="terminal-body">
+            <div className="ascii-title">
+              {/* Get In Touch */}
+              <div className="word">
+                <pre>
+                 &nbsp;_____<br/>
+                 / ____|<br/>
+                | |  _<br/>
+                | | |_|<br/>
+                | |__||<br/>
+                 \_____|
+              </pre>
+              <pre>
+                &nbsp;_____ <br/>
+                |  ___|<br/>
+                | |__  <br/>
+                |  __| <br/>
+                | |___ <br/>
+                |_____|<br/>
+              </pre>    
+              <pre>
+                &nbsp;_______ <br/>
+                |__   __|<br/>
+                  &nbsp;&nbsp;&nbsp;| |   <br/>
+                  &nbsp;&nbsp;&nbsp;| |   <br/>
+                  &nbsp;&nbsp;&nbsp;| |   <br/>
+                  &nbsp;&nbsp;&nbsp;|_|
+              </pre>
+              </div>
+              <div className="word">
+                <pre>
+                  &nbsp;_____<br/>
+                  |_   _|<br/>
+                  &nbsp;&nbsp;| |  <br/>
+                  &nbsp;&nbsp;| |  <br/>
+                  &nbsp;_| |_<br/>
+                  |_____| 
+                </pre>
+                <pre>
+                  &nbsp;_   _ <br />
+                  | \ | |<br />
+                  |  \| |<br />
+                  | . ` |<br />
+                  | |\  |<br />
+                  |_| \_| <br />
+                </pre>
+              </div>
+              <div className="word">
+                <pre>
+                &nbsp;_______ <br/>
+                |__   __|<br/>
+                  &nbsp;&nbsp;&nbsp;| |   <br/>
+                  &nbsp;&nbsp;&nbsp;| |   <br/>
+                  &nbsp;&nbsp;&nbsp;| |   <br/>
+                  &nbsp;&nbsp;&nbsp;|_|
+                </pre>
+                <pre>
+                  &nbsp;____ <br />
+                 / __ \ <br />
+                ||  ||<br />
+                ||  ||<br />
+                ||__||<br />
+                 \____/   
+                </pre>
+                <pre>
+                  &nbsp;_    _ <br />
+                  | |  | |<br />
+                  | |  | |<br />
+                  | |  | |<br />
+                  | |__| |<br />
+                  &nbsp;\____/  
+                </pre>
+                <pre>
+                  &nbsp;_____ <br />
+                  / ____|<br />
+                 | |     <br />
+                 | |     <br />
+                 | |___ <br />
+                  \_____| 
+                </pre>
+                <pre>
+                  &nbsp;_    _ <br />
+                  | |  | |<br />
+                  | |__| |<br />
+                  |  __  |<br />
+                  | |  | |<br />
+                  |_|  |_|
+                </pre>
+              </div>
+            </div>
+            <hr />
+            <hr />
+            <form className="input-form">
+              <div className="field">
+                <label htmlFor="name">
+                  > Name
+                </label>
+                <div className="input-div">$ <input type="text" name='name' id='input-name' placeholder='enter your name' required/></div>
+              </div>
+              <div className="field">
+                <label htmlFor="email">
+                  > Email
+                </label>
+                  <div className="input-div">
+                    $ <input type="email" name="email" id="input-email" placeholder='enter your email' required/>
+                  </div>
+              </div>
+              <div className="field">
+                <label htmlFor="subject">
+                  > Subject
+                </label>
+                <div className="input-div">
+                  $ <input type="text" name='subject' id='input-subject' placeholder='type here..' required/>
+                </div>
+              </div>
+              <div className="feild">
+                <label htmlFor="message">
+                  > Message
+                </label>
+                <div className="input-div">
+                  $ <textarea name="message" id="input-message" required placeholder='enter your message'></textarea>
+                </div>
+              </div>
+              <button className="submit-btn" type='submit'>Send</button>
+            </form>
           </div>
-        <img src={map} alt="" className='map'/>
+        </div>
+      </div>
+      <div className="copyright">
+        <div className="time">Bengaluru, Karnataka<br/>{time.toLocaleTimeString()}</div>
+        <p>Made by Shreya Mohanty <br />Copyright © 2025 - All Rights Reserved</p>
       </div>
     </div>
   )
